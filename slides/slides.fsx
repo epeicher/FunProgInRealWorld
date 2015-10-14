78a4d71b79d0b82a2b1fa5261c1721d034d87ff0
epeicher <ese.rober@gmail.com>
2015-10-14 08:30:27 +0200

    Table of Contents

---
+++ slides/slides.fsx
@@ -0,0 +1,66 @@
+﻿(**
+- title : FunProgInRealWorld
+- description : Introducción a la Programación Funcional
+- author : Roberto Aranda López
+- theme : moon
+- transition : default
+
+***
+
+### Programación Funcional
+- Qué es
+- Por qué me debería interesar
+
+---
+
+### Introducción a F#
+- Un poquitín de historia
+- Por qué F#
+- Características principales
+
+---
+
+### Enséñame el código
+- Sintaxis 
+- Inferencia de tipos
+- Definición de tipos
+- *Pattern matching*
+- *Currying* y Aplicaciones Parciales
+- Composición y *Pipelining*
+- Proveedores de tipos
+- Programación Asíncrona
+- Expresiones Computacionales
+
+***
+
+# Demonstraciones
+
+***
+
+### Conectándonos al Banco Mundial en 5 minutos
+
+***
+
+### Diseño Orientado al Dominio
+
+***
+
+### Nuestras viejas amigas las Bases de Datos
+
+***
+
+### Voy a mandar un Tweet
+
+***
+
+### Qué es eso de *Machine Learning*
+
+***
+
+# Resumen
+
+***
+
+# ¿Preguntas?
+
+*)
